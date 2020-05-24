157aeeedd5e8ac5b088d468da3d796ce6570ac8f
henriquegoncalves <henrique.goncalves_pj@simplifiquegp.com.br>
2020-05-24 17:06:31 -0300

    V0.1
    Some bugs don't fixed:
    
    Jump
    
    Animations
    
    Shoot

---
+++ Assets/Gothicvania/Script/Bullet.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    [SerializeField] int damage;
+
+    [SerializeField] Rigidbody rig;
+
+    [SerializeField] float speed;
+
+    [SerializeField] Camera cam;
+
+    [SerializeField] Sprite _sprite;
+
+
+    private void Start()
+    {
+
+        rig = GetComponent<Rigidbody>();
+        if (!GetComponent<SpriteRenderer>().flipX)
+        {
+            rig.velocity = transform.right * speed;
+        }
+        else if (GetComponent<SpriteRenderer>().flipX)
+        {
+            rig.velocity = transform.right * -speed;
+
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (!GetComponent<SpriteRenderer>().flipX)
+        {
+            rig.velocity = transform.right * speed;
+        }
+        else if (GetComponent<SpriteRenderer>().flipX)
+        {
+            rig.velocity = transform.right * -speed;
+
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Bug")
+        {
+           
+            Destroy(this.gameObject);
+        }
+
+        else if (other.gameObject.tag == "Enemy")
+        {
+            other.gameObject.GetComponent<Enemy_Gothicvania>().Get_Life(damage);
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Physics.IgnoreLayerCollision(15, 17);
+
+        if (collision.gameObject.layer == 12)
+        {
+
+            Destroy(this.gameObject);
+
+        }
+
+
+
+    }
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+        Debug.Log("Destroyed");
+    }
+
+
+
+    public void EffectBullet()
+    {
+        Destroy(this.gameObject);
+       
+    }
+
+
+
+}
---
+++ Assets/Gothicvania/Script/Plataform.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plataform : MonoBehaviour
+{
+
+    [SerializeField] float speed;
+
+    [SerializeField] Transform destination;
+
+    [SerializeField] GameObject destinationFirst;
+
+    [SerializeField] GameObject destinationSecond;
+
+    [SerializeField] bool changeDestination;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+        transform.position = Vector3.MoveTowards(this.transform.position, destination.position, speed * Time.deltaTime);
+
+
+        if (this.transform.position == destination.transform.position && !changeDestination)
+        {
+
+            destination = destinationSecond.transform;
+            changeDestination = true;
+        }
+
+        else if (this.transform.position == destination.transform.position && changeDestination)
+        {
+            destination = destinationFirst.transform;
+            changeDestination = false;
+
+
+        }
+
+
+
+
+
+
+    }
+}
